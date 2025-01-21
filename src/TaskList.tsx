c8514cb574e5004cd1a3d840e2d5fdb22eacaed7
johannes.doellinger <johannes.doellinger@gmail.com>
2025-01-21 15:39:41 +0000

    Migrate TaskList as well

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,46 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Task, Alignment } from './types';
 import TaskDetailComponent from './TaskDetailComponent';
+import { List, ListItem, ListItemText, Typography, Box, Paper } from '@mui/material';
 
 interface TaskListProps {
-    tasks: Task[];
-    alignments: Alignment[];
-    selectedTask: Task | null;
-    setSelectedTask: React.Dispatch<React.SetStateAction<Task | null>>;
-    setSecondTask: React.Dispatch<React.SetStateAction<Task | null>>;
-    setAlignments: React.Dispatch<React.SetStateAction<Alignment[]>>;
-  }
-  
-  const TaskList: React.FC<TaskListProps> = ({ tasks, alignments, selectedTask, setSelectedTask, setSecondTask, setAlignments }) => {
-  
-    return (
-      <div>
-        <h2 className="text-2xl text-white mb-4">Task List</h2>
-        <ul>
-          {tasks.map((task) => (
-            <li
-              key={task.id}
-              className="text-white bg-gray-700 p-4 rounded-lg mb-4 cursor-pointer"
-              onClick={() => setSelectedTask(task)}
-            >
-              {task.name}
-            </li>
-          ))}
-        </ul>
-  
-        {selectedTask && (
-          <TaskDetailComponent
-            task={selectedTask}
-            tasks={tasks}
-            alignments={alignments}
-            setTasks={() => {}}
-            setAlignments={setAlignments}
-            deleteTask={() => {}}
-          />
-        )}
-      </div>
-    );
-  };
-  
-  export default TaskList;
-  
+  tasks: Task[];
+  alignments: Alignment[];
+  selectedTask: Task | null;
+  setSelectedTask: React.Dispatch<React.SetStateAction<Task | null>>;
+  setSecondTask: React.Dispatch<React.SetStateAction<Task | null>>;
+  setAlignments: React.Dispatch<React.SetStateAction<Alignment[]>>;
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, alignments, selectedTask, setSelectedTask, setAlignments }) => {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h4" color="white" gutterBottom>
+        Task List
+      </Typography>
+      <List>
+        {tasks.map((task) => (
+          <ListItem
+            key={task.id}
+            component={Paper}
+            sx={{
+              bgcolor: 'grey.800',
+              color: 'white',
+              mb: 2,
+              cursor: 'pointer',
+              '&:hover': { bgcolor: 'grey.700' },
+              borderRadius: 2,
+            }}
+            onClick={() => setSelectedTask(task)}
+          >
+            <ListItemText primary={task.name} />
+          </ListItem>
+        ))}
+      </List>
+
+      {selectedTask && (
+        <TaskDetailComponent
+          task={selectedTask}
+          tasks={tasks}
+          alignments={alignments}
+          setTasks={() => { }}
+          setAlignments={setAlignments}
+          deleteTask={() => { }}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default TaskList;
